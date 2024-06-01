f7c9ab818da0caf85fd33e6b602b87758f10ef40
NikolasZM <101744161+NikolasZM@users.noreply.github.com>
2024-05-31 20:20:38 -0500

    Update Cintas.cpp

--- Cintas.cpp
+++ Cintas.cpp
@@ -3,16 +3,26 @@
 #include <algorithm>
 using namespace std;
 
-double PromAcceso(std::vector<int>& archivos) {
-    sort(archivos.begin(), archivos.end());
+double PromAcceso(vector<int>& archivos, vector<int>& orden) {
+    
+    vector<int> indices(archivos.size());
+
+    for (int i{ 0 }; i < archivos.size(); i++) {
+        indices[i] = i;
+    }
+
+    sort(indices.begin(), indices.end(), [&](int a, int b) {
+        return archivos[a] < archivos[b];
+        });
 
     int n = archivos.size();
     int tiempoTotal = 0;
     int tiempoAcumulado = 0;
 
     for (int i = 0; i < n; ++i) {
-        tiempoAcumulado += archivos[i];
+        tiempoAcumulado += archivos[indices[i]];
         tiempoTotal += tiempoAcumulado;
+        orden.push_back(indices[i] + 1);
     }
 
     return static_cast<double>(tiempoTotal) / n;
@@ -20,10 +30,14 @@ double PromAcceso(std::vector<int>& archivos) {
 
 int main() {
     vector<int> archivos = { 5, 10, 3 };
+    vector<int> orden;
+    double tiempoPromedio = PromAcceso(archivos,orden);
 
-    double tiempoPromedio = PromAcceso(archivos);
-
-    cout << "Tiempo promedio de acceso: " << tiempoPromedio << std::endl;
+    cout << "Tiempo promedio de acceso: " << tiempoPromedio << endl;
+    cout << "El orden de las tareas es: " ;
+    for (int i = 0; i < archivos.size(); i++) {
+        cout << orden[i] << " - ";
+    }
 
     return 0;
 }
